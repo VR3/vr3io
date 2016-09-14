ea2b75ad5f38ff764f28f1f925e2a918792cc10d
ostocino <oscarchavezromero@gmail.com>
2016-09-13 22:53:02 -0500

    Initial Commit

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,69 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+console.log('VR3.io | Video Infraestructure for the Web and VR. Version 1.4.0RoR')
+
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$(document).ready(function(){
+  $('.clients').slick({
+    slidesToShow: 5,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  draggable: true,
+	  pauseOnHover: true,
+	  prevArrow: false,
+	  nextArrow: false,
+	  autoplaySpeed: 3000,
+	  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+});
