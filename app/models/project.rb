19b9225319dff672c762104682ccac1a6c4ae4f4
oschvr <oscarchavezromero@gmail.com>
2017-07-13 23:58:31 -0500

    Trying to fix a paperclip bug in Heroku

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,8 +6,8 @@ class Project < ApplicationRecord
     extend FriendlyId
 	friendly_id :slug_candidates, :use => [:slugged, :finders]
 
-	#has_attached_file :image, styles: { standard: "562x387>", thumb: "281x194>" }, default_url: "http://static.vr3.io/vr3/img/main/projectmissing.png"
-    #validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	has_attached_file :image, styles: { standard: "562x387>", thumb: "281x194>" }, default_url: "http://static.vr3.io/vr3/img/main/projectmissing.png"
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
     Paperclip.interpolates :client do |a,s|
         a.instance.client.downcase
@@ -17,5 +17,4 @@ class Project < ApplicationRecord
 		[:title, [:title, :client], [:title, :client, :provider]]
     end
     
-
 end
