5de86668329558a7a5e8d51ec8f16b6cb0bd2e59
oschvr <oscarchavezromero@gmail.com>
2017-07-19 17:05:09 -0500

    Fix bug in S3 REGION for environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,15 +50,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
   # Paperclip AWS S3
   config.paperclip_defaults = {
     storage: :s3,
+    s3_region: ENV.fetch('AWS_REGION')
     s3_credentials: {
       bucket:  ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
       s3_host_name: ENV.fetch('AWS_S3_HOST_NAME')
     }
   }
