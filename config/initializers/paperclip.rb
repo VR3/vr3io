4b3083c7f0e41f781199d4dce69b23d6b02288d8
oschvr <oscarchavezromero@gmail.com>
2017-07-19 16:25:29 -0500

    Added figaro gem to manage env vars. Added AWS S3 storage to paperclip. RW

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 require 'paperclip'
 #include Paperclip::Schema
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
-Paperclip::Attachment.default_options[:url] = ':vr3iodev'
+Paperclip::Attachment.default_options[:url] = ':vr3io'
 Paperclip::Attachment.default_options[:path] = '/projects/:client/:attachment/:style/:filename'
