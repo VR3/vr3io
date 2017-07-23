af83cb22d00751585837c7480a31c64b42f2d365
oschvr <oscarchavezromero@gmail.com>
2017-07-23 03:06:31 -0500

    Add https protocol support for S3 images

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,11 +4,11 @@ class Project < ApplicationRecord
     extend FriendlyId
 	friendly_id :slug_candidates, :use => [:slugged, :finders]
 
-    has_attached_file :image, :styles => { standard: "562x387>", thumb: "281x194>" }, :default_url => "https://s3-us-west-2.amazonaws.com/vr3io/projectmissing.png", :storage => :s3, :s3_region => ENV['AWS_REGION'], :bucket => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :url => "https://s3-us-west-2.amazonaws.com/vr3io/projects/:client/:attachment/:style/:filename"
+    has_attached_file :image, :styles => { standard: "562x387>", thumb: "281x194>" }, :default_url => "https://s3-us-west-2.amazonaws.com/vr3io/projectmissing.png", :storage => :s3, :s3_protocol => 'https', :s3_region => ENV['AWS_REGION'], :bucket => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :url => "https://s3-us-west-2.amazonaws.com/vr3io/projects/:client/:attachment/:style/:filename"
 
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-    has_attached_file :video, :styles => { :medium => { :geometry => "640x480", :format => 'mp4' }, :thumb => { :geometry => "160x120", :format => 'jpeg', :time => 10 } }, :processors => [:transcoder], :default_url => "https://s3-us-west-2.amazonaws.com/vr3io/projectmissing.png", :storage => :s3, :s3_region => ENV['AWS_REGION'], :bucket => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :url => "https://s3-us-west-2.amazonaws.com/vr3io/projects/:client/:attachment/:style/:filename"
+    has_attached_file :video, :styles => { :medium => { :geometry => "640x480", :format => 'mp4' }, :thumb => { :geometry => "160x120", :format => 'jpeg', :time => 10 } }, :processors => [:transcoder], :default_url => "https://s3-us-west-2.amazonaws.com/vr3io/projectmissing.png", :storage => :s3, :s3_protocol => 'https', :s3_region => ENV['AWS_REGION'], :bucket => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :url => "https://s3-us-west-2.amazonaws.com/vr3io/projects/:client/:attachment/:style/:filename"
     
     validates_attachment_content_type :video, content_type: /\Avideo\/.*\Z/
 
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,3 +4,4 @@ Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.c
 Paperclip::Attachment.default_options[:url] = ':vr3io'
 Paperclip::Attachment.default_options[:path] = '/projects/:client/:attachment/:style/:filename'
 Paperclip::Attachment.default_options[:region] = 'us-west-2'
+Paperclip::Attachment.default_options[:s3_protocol] = 'https'
