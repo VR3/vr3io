855e2771ba2a61f4d7e751e5b8efcda67f25db28
oschvr <oscarchavezromero@gmail.com>
2017-09-04 10:53:55 -0500

    Adding static script in php for contact, Testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root "pages#show", page: "home"
 
   get "company" => "pages#company"
   get "privacy" => "pages#privacy"
   get "services" => "pages#services"
-  get "taxes" => "pages#taxes"
+  get "contact" => "pages#contact"
 
   resources "projects"
   resources "jobs"
